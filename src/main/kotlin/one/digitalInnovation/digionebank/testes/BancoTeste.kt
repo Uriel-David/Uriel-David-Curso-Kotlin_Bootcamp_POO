db785682eb521d62a61f360e557fc1f93ff97117
Uriel-David <uriel.david.qaa@gmail.com>
2021-03-13 16:13:50 -0300

    Criação do arquivo "BancoTeste" para testar o Data Class "Banco".

---
+++ src/main/kotlin/one/digitalInnovation/digionebank/testes/BancoTeste.kt
@@ -0,0 +1,14 @@
+package one.digitalInnovation.digionebank.testes
+
+import one.digitalInnovation.digionebank.Banco
+
+fun main() {
+    val digiOneBank = Banco(nome = "DigiOne", numero = 12)
+
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+    val banco2 = digiOneBank.copy(nome = "Banco2")
+
+    println(banco2.info())
+}
